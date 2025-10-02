e01f6e5e05e361f0ae445ba343d5007c586b2982
FlavioKde <flaviodavirro@gmail.com>
2025-10-02 13:37:21 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.chatbot"
+version = "0.0.1-SNAPSHOT"
+description = "Chatbot building challenge, to handle a company's onboarding"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	// SpringDoc OpenAPI
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+
+	// Logback
+	implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+
+	//Test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
